5ebcdc435687006ba8766d008b3409b1f5c97253
ProgramBoon <42967429+ProgramBoon@users.noreply.github.com>
2021-05-28 17:42:13 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import psutil
+import sys
+sys.path.append('/home/user/PycharmProjects/monitor1/Keenup.lib')
+import sne
+
+
+sne.Sne._getInfo()
+
+sne.Sne._toPrint()
+
+sne.SneToString._PrintToString()
+
+
+# deviceInfo = dict()
+# deviceInfo["cpu_times"] = psutil.cpu_times()._asdict()
