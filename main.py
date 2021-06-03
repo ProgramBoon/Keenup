e1c1cd76b8d9e9db562acc6fdd7a9498b28c2cdf
ProgramBoon <42967429+ProgramBoon@users.noreply.github.com>
2021-06-03 10:34:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import sys
 import os
 sys.path.append(os.getcwd()+'/Keenup.lib')
 import sne
-
+import database
+import cre2
 
 
 
@@ -10,11 +11,14 @@ sne.Sne._getInfo()
 
 sne.Sne._toPrint()
 
+# делаем хмл
+p = cre2.XML('ui')
+p.createFile()
+# проверяем подключение - ничего, значит ок, ошибка - не подключились
+p = database.Database()
+
 
 # sne.SneToString._PrintToString()
 
 
 
-
-# deviceInfo = dict()
-# deviceInfo["cpu_times"] = psutil.cpu_times()._asdict()
