707c2064d38ef1cc7c79c44dd9ebe255d84d9fa8
ProgramBoon <42967429+ProgramBoon@users.noreply.github.com>
2021-06-11 16:24:20 +0700

    Update sne.py

--- Keenup.lib/sne.py
+++ Keenup.lib/sne.py
@@ -5,102 +5,118 @@ import ast
 
 
 class Sne(object):
-    _info = []
+    _info = {}
 
 
+    def dict_add_keep_last(a, b):  # aka merged() or updated()
+        merged = {a, b}
+        return merged
 
-    @staticmethod
-    def getProcI():   # get processor info
-        proc = [] #storage list
-        proc.append(psutil.cpu_times()._asdict()) #gets scputimes(user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice)
+    @classmethod
+    def getProcI(self):   # get processor info
+        proc = {} #storage list
+        proc = (psutil.cpu_times()._asdict()) #gets scputimes(user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice)
 
-        proc.append({psutil.cpu_percent()}) #shows cpu load
+        # proc.append('cpu_percent:')
+        x = {'cpu_percent': psutil.cpu_percent()} #shows cpu load
+        proc = {**proc, **x}
+        # x('cpu_times_percent')
+        x = (psutil.cpu_times_percent()._asdict()) #shows procentile cpu load
 
-        proc.append('cpu_times_percent')
-        proc.append(psutil.cpu_times_percent()._asdict()) #shows procentile cpu load
+        for key in list(x.keys()):
+            x[key +'_percent'] = x.pop(key)
 
-        # логические
-        proc.append('cpu_count(logical=True)')
-        a = psutil.cpu_count(logical=True)
-        proc.append(ast.literal_eval(str(a))) #shows quantity of cpu log cores
+        proc = {**proc, **x}
+         # логические
+        x = {'cpu_count(logical)': psutil.cpu_count(logical=True)}
+        proc = {**proc, **x}
+        # # физические
+        x = {'cpu_count(physical)': psutil.cpu_count(logical=False)}
+        proc = {**proc, **x}
 
+        proc = {**proc, **psutil.cpu_stats()._asdict()}#shows cpu stats: ctx_switches, interrupts, soft_interrupts, syscalls
+        # # частота цпу
 
-        # физические
-        proc.append('cpu_count(logical=f)')
-        a = psutil.cpu_count(logical=False)
-        proc.append(ast.literal_eval(str(a))) #shows quantity of cpu real cores
-
-
-        proc.append(psutil.cpu_stats()._asdict()) #shows cpu stats: ctx_switches, interrupts, soft_interrupts, syscalls
-
-        # частота цпу
-        proc.append(psutil.cpu_freq()._asdict()) #cpu freq current, min, max
-
-        # поядерная
-        proc.append(dict.fromkeys(psutil.cpu_freq(percpu=True))) #cpu freq for each core
+        proc = {**proc, **psutil.cpu_freq()._asdict()} #cpu freq current, min, max
+        #
+        # # поядерная
+        proc = {**proc, **dict.fromkeys(psutil.cpu_freq(percpu=True))} #cpu freq for each core
 
 
-        dictionary = {} #cpu load (%)
-        q = psutil.getloadavg(), dictionary
-        proc.append(q)
+        q = psutil.getloadavg()
+        proc = {**proc, **{'loadavg1':q[0],'loadavg5':q[1],'loadavg15':q[2]}}
+
         # в процентах
         q = [x / psutil.cpu_count() * 100 for x in psutil.getloadavg()]
-        proc.append(dict.fromkeys(q))
+        proc = {**proc, **{'loadpercent1':q[0],'loadpercent5':q[1],'loadpercent15':q[2]}}
         return proc
 
     @staticmethod
     def getMemI(): #get memory info
-        mem = []
-        mem.append(psutil.virtual_memory()._asdict())
-        mem.append(psutil.swap_memory()._asdict())
-        mem.append(psutil.Process().memory_full_info()._asdict())
-
+        mem = {}
+        mem = {**mem, **psutil.virtual_memory()._asdict()}
+        mem = {**mem, **psutil.swap_memory()._asdict()}
+        mem = {**mem, **psutil.Process().memory_full_info()._asdict()}
         return mem
 
     @staticmethod
     def getDiscI(): #get disc info
-        disc = []
+        disc = {}
 
         # disc.append(dict.fromkeys(psutil.disk_partitions()))
-        disc.append(psutil.disk_io_counters()._asdict())
+        disc = {**disc, **psutil.disk_io_counters()._asdict()}
         # disc.append(psutil.disk_io_counters(perdisk=True))
 
         return disc
 
     @staticmethod
     def getNetI():#get net info
-        net = []
-        net.append(psutil.net_io_counters()._asdict())
+        net = {}
+        net = {**net, **psutil.net_io_counters()._asdict()}
         # net.append(psutil.net_io_counters(pernic=True))
 
-        net.append(dict.fromkeys(psutil.net_connections()))
+        net = {**net, **dict.fromkeys(psutil.net_connections())}
 
 
-        net.append(psutil.net_if_addrs())
-        net.append(psutil.net_if_stats())
+        net = {**net, **psutil.net_if_addrs()}
+        net = {**net, **psutil.net_if_stats()}
         return net
 
-    @staticmethod
-    def getSensI():#get temperature and fans info
-        sen = []
-        sen.append(psutil.sensors_temperatures(fahrenheit=False))
-        sen.append(psutil.sensors_fans())
-        sen.append(psutil.sensors_battery())
-        return sen
+    # @staticmethod
+    # def getSensI():#get temperature and fans info
+    #     sen = {}
+    #     x = (psutil.sensors_temperatures(fahrenheit=False))
+    #     q = {}
+    #     for val in list(x.values()):
+    #         for item in len(val):
+    #             q =
+    #
+    #
+    #
+    #     sen = {**sen, **psutil.sensors_temperatures(fahrenheit=False)}
+    #     sen = {**sen, **psutil.sensors_fans()}
+    #
+    #     sen = {**sen, **{'battery': psutil.sensors_battery()}}
+    #     return
 
     @classmethod
     def _getInfo(self): #get all info
-        self._info.append(self.getProcI())
-        self._info.append(self.getMemI())
-        self._info.append(self.getDiscI())
-        self._info.append(self.getNetI())
-        self._info.append(self.getSensI())
+        self._info = {**self._info, **self.getProcI()}
+        self._info = {**self._info, **self.getMemI()}
+        # self._info = {**self._info, **self.getDiscI()}
+        # self._info = {**self._info, **self.getNetI()}
+        # self._info = {**self._info, **self.getSensI()}
 
 
     @classmethod
     def _toPrint(self): #prints info
         print(self._info)
-        self._info = []
+        self._info = {}
+
+    @classmethod
+    def _ret(self): #prints info
+        return (self._info)
+        self._info = {}
 
 
 class SneToString(Sne): #prints info as string
