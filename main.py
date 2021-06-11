3d8c7957aa5681df36ae3135712621ba0be0666b
ProgramBoon <42967429+ProgramBoon@users.noreply.github.com>
2021-06-11 16:22:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,26 @@ sys.path.append(os.getcwd()+'/Keenup.lib')
 import sne
 import database
 import cre2
+import cron
+import getpass
+from tmpw import app
 
-
-
-sne.Sne._getInfo()
-
-sne.Sne._toPrint()
 
 # делаем хмл
 p = cre2.XML('ui')
 p.createFile()
+# period in cre2
+getinf = cron.ScheduleManager()
+getinf.run()
+
+
+
+
 # проверяем подключение - ничего, значит ок, ошибка - не подключились
-p = database.Database()
-
-
+# p = database.Database()
+# # p.insert_data(sne.Sne._ret())
+# p.alarm()
+app.run()
 # sne.SneToString._PrintToString()
 
 
