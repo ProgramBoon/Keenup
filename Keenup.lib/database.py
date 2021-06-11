2502daf197b2458259941640c2385926f7d09404
ProgramBoon <42967429+ProgramBoon@users.noreply.github.com>
2021-06-11 16:23:18 +0700

    Update database.py

--- Keenup.lib/database.py
+++ Keenup.lib/database.py
@@ -1,12 +1,14 @@
 import psycopg2
 import cre2
+import datetime
+import Keenbot
 
 class Database:
     _conn = 0
     def __init__(self):
 
         p = cre2.XML('ui')
-        dbn =  p.parsingFile("database",True)
+        dbn =p.parsingFile("database",True)
         us = p.parsingFile("user",True)
         pas = p.parsingFile("password",True)
         hos = p.parsingFile("host",True)
@@ -17,16 +19,27 @@ class Database:
 
     def insert_data(self, data):
         """ insert multiple vendors into the vendors table  """
-        sql = "INSERT INTO datacatch(pcid, keyname, value) VALUES(1, %s, %s)"
-
+        sql = "INSERT INTO datacatch(keyname , value) VALUES(%s, %s)"
+        datesql = "INSERT INTO public.date(date) VALUES(%s);"
         self.conn = None
         try:
+            p = cre2.XML('ui')
+            dbn = p.parsingFile("database", True)
+            us = p.parsingFile("user", True)
+            pas = p.parsingFile("password", True)
+            hos = p.parsingFile("host", True)
+
+            date_object = "\'"+str(datetime.date.today())+"\'"
+            print(date_object)
+
+            try:
+                self.conn = psycopg2.connect(dbname=dbn, user=us, password=pas, host=hos)
 
                 # create a new cursor
                 cur = self.conn.cursor()
-
+                cur.execute(datesql, (date_object,))
                 for i in data:
-                    cur.execute(sql, (i, data[i]))
+                      cur.execute(sql, (i, data[i]))
 
                 # commit the changes to the database
                 self.conn.commit()
@@ -38,4 +51,54 @@ class Database:
             if self.conn is not None:
                 self.conn.close()
 
+    def alarm(self):
+        sql = "SELECT * FROM public.log;"
+        sqlupdate = "UPDATE public.log SET alarm = FALSE WHERE alarm = 'TRUE';"
+        self.conn = None
+        try:
+            p = cre2.XML('ui')
+            dbn = p.parsingFile("database", True)
+            us = p.parsingFile("user", True)
+            pas = p.parsingFile("password", True)
+            hos = p.parsingFile("host", True)
+            try:
+                self.conn = psycopg2.connect(dbname=dbn, user=us, password=pas, host=hos)
+                # create a new cursor
+                cur = self.conn.cursor()
+                cur.execute(sql)
+                x = cur.fetchall()
+                for i in x:
+                    if i[3] is True:
+                        tex = 'тревога в ' + str(i[4])+' сенсор '+str(i[0])+' время '+ str(i[1])+ ' id в datacatch: '+ str(i[2])
+                        Keenbot.send_telegram(tex)
+                        cur.execute(sqlupdate)
+                self.conn.commit()
 
+            except (Exception, psycopg2.DatabaseError) as error:
+                print(error)
+        finally:
+            if self.conn is not None:
+                self.conn.close()
+
+    def alarmlast(self):
+        sql = "SELECT * FROM (SELECT * FROM log ORDER BY id DESC LIMIT 10) sub ORDER BY id ASC;"
+        self.conn = None
+        try:
+            p = cre2.XML('ui')
+            dbn = p.parsingFile("database", True)
+            us = p.parsingFile("user", True)
+            pas = p.parsingFile("password", True)
+            hos = p.parsingFile("host", True)
+            try:
+                self.conn = psycopg2.connect(dbname=dbn, user=us, password=pas, host=hos)
+                # create a new cursor
+                cur = self.conn.cursor()
+                cur.execute(sql)
+                x = cur.fetchall()
+                self.conn.commit()
+                return(x)
+            except (Exception, psycopg2.DatabaseError) as error:
+                print(error)
+        finally:
+            if self.conn is not None:
+                self.conn.close()
